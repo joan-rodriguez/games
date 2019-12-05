97e01b931eb2df5e45c50eb78357cc2d74c8924a
joan <joan.rodriguezre@gmail.com>
2019-12-05 17:12:02 +0100

    Paddles working fine!

--- pong_game/pong/ball.py
+++ pong_game/pong/ball.py
@@ -8,9 +8,12 @@ class Ball:
         self.ball = turtle.Turtle()     # Creates "Turtle object".
 
         """Settings:"""
-        self.ball.speed(0)            # Sets paddle speed (0 = Max speed).
-        self.ball.shape("square")     # Sets paddle shape.
-        self.ball.color("white")      # Sets paddle color.
+        self.ball.speed(0)              # Sets paddle speed (0 = Max speed).
+        self.ball.shape("square")       # Sets paddle shape.
+        self.ball.color("white")        # Sets paddle color.
 
-        self.ball.penup()             # To avoid drawing line while moving.
-        self.ball.goto(0, 0)          # Sets starting position.
+        self.ball.penup()               # To avoid drawing line while moving.
+        self.ball.goto(0, 0)            # Sets starting position.
+
+        self.ball.dx = 5                # Horizontal movement.
+        self.ball.dy = 5                # Vertical movement.
--- pong_game/pong/game.py
+++ pong_game/pong/game.py
@@ -6,31 +6,33 @@ from ball import Ball
 
 def create_window(title):
     """Creates a window with specific settings."""
-    wn = turtle.Screen()                    # Creates one window
+    win = turtle.Screen()                    # Creates one window
 
     """Settings:"""
-    wn.title(title)                         # Sets title.
+    win.title(title)                         # Sets title.
 
-    wn.bgcolor("black")                     # Background color.
-    wn.setup(width=800, height=600)         # Width and height in pixels.
+    win.bgcolor("black")                     # Background color.
+    win.setup(width=800, height=600)         # Width and height in pixels.
 
-    wn.tracer(0)
+    win.tracer(0)
+    win.listen()
 
-    return wn
+    return win
 
 wn = create_window("Pong")
 
 # Paddle A
 paddle_a = Paddle("A", -350, 0)
+wn.onkeypress(paddle_a.paddle_up, "w")
+wn.onkeypress(paddle_a.paddle_down, "s")
 
 # Paddle B
 paddle_b = Paddle("B", 350, 0)
+wn.onkeypress(paddle_b.paddle_up, "Up")
+wn.onkeypress(paddle_b.paddle_down, "Down")
 
 # Ball
 ball = Ball()
-
-wn.listen()
-wn.onkeypress(paddle_a.paddle_up(), "w")
 
 while True:
     wn.update()
