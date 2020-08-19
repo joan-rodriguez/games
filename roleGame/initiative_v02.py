824143dc4198fab23e6811112709cc6a32490e5a
joan <joan@joan-pc.vencasens>
2020-08-19 18:51:53 +0200

    Some actions pending in Menu

--- roleGame/initiative_v02.py
+++ roleGame/initiative_v02.py
@@ -34,6 +34,8 @@ class Manager:
     def __init__(self):
         # combat container is generated:
         self.combat = []
+        self.round = 0
+        self.player_number = 0
 
         # Options for menu are generated:
         # self.options = {0: self.general_status, 1: self.attack,
@@ -42,10 +44,9 @@ class Manager:
 
         self.initialize_combat()
 
-        print(self.combat)
-        self.display_menu()
-        self.attack(self.combat[0])
-        self.general_status()
+        self.options = {0: self.general_status, 1: self.attack,
+                        5: self.add_combatant}
+        self.player = self.combat[self.player_number]
 
     def add_combatant(self, combatant):
         # This function adds a new combatant
@@ -83,61 +84,79 @@ class Manager:
         # Attacker attacks to an individual in combat.
         # Following part identifies individual being attacked:
 
-        injured = check_name(input('Who is {} attacking to? --> '.format(attacker[1])), self.combat)
         while True:
-            if injured != 'No one':
+            injured = check_name(input('Who is {} attacking to? (Enter to skip) --> '.format(
+                attacker[1])), self.combat)
+
+            if injured == '':
                 break
-            injured = check_name(input('This individual is not part of the combat. Who is {} attacking '
-                                       'to? --> '.format(attacker[1])), self.combat)
 
-
-        # Following part checks damage and subtracts it from PGs:
-        damage = check_number(input('How much damage does {} do? --> '.format(attacker[1])), 'Damage')
-        self.combat[injured][3] -= damage
-
-        print('{}\'s PG are now {}!!'.format(self.combat[injured][1], self.combat[injured][3]))
-
-        # It also has into account if resulting PG are negative:
-        if self.combat[injured][3] < 0:
-            dead = 'Maybe'
             while True:
-                if dead.upper() == 'Y':
-                    self.erase_combatant(injured)
-                    print()
+                if injured != 'No one':
                     break
-                elif dead.upper() == 'N':
-                    print()
-                    break
-                else:
-                    dead = input(
-                        'Is {} dead with {} PG? (Y/N) --> '.format(self.combat[injured][1], self.combat[injured][3]))
+                injured = check_name(input('This individual is not part of the combat. Who is {} attacking '
+                                           'to? (\'No one\' to continue) --> '.format(attacker[1])), self.combat)
+
+
+
+            # Following part checks damage and subtracts it from PGs:
+            damage = check_number(input('How much damage does {} do? --> '.format(attacker[1])), 'Damage')
+            self.combat[injured][3] -= damage
+
+            print('{}\'s PG are now {}!!'.format(self.combat[injured][1], self.combat[injured][3]))
+
+            # It also has into account if resulting PG are negative:
+            if self.combat[injured][3] < 0:
+                dead = 'Maybe'
+                while True:
+                    if dead.upper() == 'Y':
+                        self.erase_combatant(injured)
+                        print()
+                        break
+                    elif dead.upper() == 'N':
+                        print()
+                        break
+                    else:
+                        dead = input(
+                            'Is {} dead with {} PG? (Y/N) --> '.format(self.combat[injured][1],
+                                                                       self.combat[injured][3]))
+
 
     def display_menu(self):
-            # Displays main menu:
+        # Displays main menu:
 
-            print("""
+        print("""
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     |   o---|==== Menu =====>   |
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     | 0- General Status         | --> OK
     |                           |
-    | 1- Attack                 | --> Are there more attacks?
+    | 1- Attack                 | --> OK
     | 2- Delay Action           | --> Pending
     | 3- Add Status             | --> Pending
     |                           |
     | 4- Conduct Delayed Action | --> Pending
     | 5- Add Combatant          | --> OK
     |                           |
-    | 6- Exit                   | --> OK
+    | 6- End Player's round     | --> Ongoing
+    | 7- Exit                   | --> OK
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    | Round --->""", self.round, end='')
+        if self.round < 10:
+            print(' '*13, '|', end='')
+        elif self.round > 9:
+            print(' ' * 12, '|', end='')
+        else:
+            print(' ' * 11, '|', end='')
+        print("""
+    | Player -->""", self.player[1], ' '*(13-len(self.player[1])),"""|
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
     """)
 
     def erase_combatant(self, row):
         self.combat.remove(self.combat[row])
         print(self.combat)
-
-    def exit_initiative(self):
-        exit()
 
     def initialize_combat(self):
         # Creates combatants and adds them to the combat container:
@@ -152,16 +171,54 @@ class Manager:
     def general_status(self):
 
         print('~'*100)
-        print('Number', ' '*(25-7), 'Name', ' '*(25-5), 'Initiative', ' '*(25-11), 'PG', ' '*(25-3))
+        print(' '*40, 'ROUND', self.round)
+        print('~' * 100)
+        print('     Number', ' '*(25-7), 'Name', ' '*(25-5), 'Initiative', ' '*(25-11), 'PG', ' '*(25-3))
         print('~' * 100)
         for i in self.combat:
+            if i == self.player:
+                print(' --> ', end='')
+            else:
+                print('     ', end='')
+
             for j in i:
                 print(j, ' '*(25-len(str(j))), end='')
             print()
         print('~' * 100)
 
+    def run(self):
+        while True:
+            self.display_menu()
+
+            option = input('What will the option be? --> ')
+
+            valid_option = None
+
+            if option.isdecimal():
+                option = int(option)
+
+                if option in self.options:
+                    valid_option = True
+
+                    self.options[option]()
+                    input('\n<enter>\n')
+
+                elif option == 6:
+                    self.player_number += 1
+
+                elif option == 7:
+                    print('\nToday\'s combat is over, but the adventure hasn\'t finished yet...\n')
+                    exit()
+
+            if not valid_option:
+                print('I\'m sorry, you need to enter one of the options (number)')
+
 
 def check_name(name, matrix):
+
+    if name == '':
+        return ''
+
     i = 0
     while i < len(matrix):
         if matrix[i][1] == name:
@@ -195,32 +252,7 @@ def check_number(value, charact='Input', name=None):
 
 
 if __name__ == '__main__':
-    Manager()
+    Manager().run()
 
 
 
-    # def run(self):
-    #     while True:
-    #         self.display_menu()
-    #
-    #         option = input('What will the option be? --> ')
-    #
-    #         valid_option = None
-    #
-    #         if option.isdecimal():
-    #             option = int(option)
-    #
-    #             if option in self.options:
-    #                 valid_option = True
-    #
-    #                 # Equivalent
-    #                 self.options[option]()
-    #
-    #                 input('\n<enter>\n')
-    #
-    #             elif option == 6:
-    #                 print('\nToday\'s combat is over, but the adventure hasn\'t finished yet...\n')
-    #                 exit()
-    #
-    #         if not valid_option:
-    #             print('I\'m sorry, you need to enter one of the options (number)')
